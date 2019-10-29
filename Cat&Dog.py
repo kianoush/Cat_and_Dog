19c26b557e50e0bb84294ce3701ada677f9cfe0d
Kian <k.karimipor@gmail.com>
2019-10-28 22:54:46 -0700

    Add resnet18 model

--- Cat&Dog.py
+++ Cat&Dog.py
@@ -53,7 +53,7 @@ val_datasets = torchvision.datasets.ImageFolder('D:/pro/data/Cat&Dog/val/', tran
                                                                                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ]))
 
-val_datasets = torchvision.datasets.ImageFolder('D:/pro/data/Cat&Dog/test/', transform=transforms.Compose([
+test_datasets = torchvision.datasets.ImageFolder('D:/pro/data/Cat&Dog/test/', transform=transforms.Compose([
                                                                                                         transforms.Resize((sz,sz)),
                                                                                                         transforms.ToTensor(),
                                                                                                         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
